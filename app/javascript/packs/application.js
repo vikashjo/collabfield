658302ae80f41fe915b51a806a0549e55158354c
vikashjo <vikashjoshi431@gmail.com>
2023-04-17 13:16:34 +0530

    Add margin-top 50px to the body

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 //= require jquery
-//= require bootstrap-sprockets
+//= require bootstrap
